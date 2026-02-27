e9325097e8e37546604c9291ffeb21e91f276476
coutinho98 <mateuscouto98@outlook.com>
2026-02-27 15:37:19 -0300

    feat(app): register auth/prisma modules and add protected test route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('fishes')
+export class FishController {
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('secret')
+  getSecretFishes(@Request() req) {
+    return {
+      message: 'Acesso liberado aos peixes raros!',
+      user: req.user,
+    };
   }
 }
