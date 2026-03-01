84064eae7b19752d8295145d0838a39b12c0e826
coutinho98 <mateuscouto98@outlook.com>
2026-03-01 04:24:01 -0300

    feat(auth): add global AtGuard, RolesGuard and PlanGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,35 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { ThrottlerModule } from '@nestjs/throttler'
+import { ThrottlerModule } from '@nestjs/throttler';
 import { SpeciesModule } from './species/species.module';
+import { AtGuard } from './auth/guard/at.guard';
+import { RolesGuard } from './auth/guard/roles.guard';
+import { PlanGuard } from './auth/guard/plan.guard';
 
 @Module({
   imports: [AuthModule, PrismaModule, ThrottlerModule.forRoot([{
-    ttl: 60000, 
-    limit: 10,  
+    ttl: 60000,
+    limit: 10,
   }]), SpeciesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AtGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: PlanGuard,
+    },
+  ],
 })
 export class AppModule { }
