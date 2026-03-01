de9bf73f95d6ff495fd4b9a58f275d50b65ae3e8
coutinho98 <mateuscouto98@outlook.com>
2026-03-01 04:24:11 -0300

    chore: setup global ValidationPipe and Helmet

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,        // Strips out any properties that do not have any decorators
+    forbidNonWhitelisted: true, // Throws an error if non-whitelisted properties are present
+    transform: true,        // Automatically transforms payloads to be objects typed according to their DTO classes
+  }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
